c419116a3eaaf8c77de7c9c2d2dd330867fbcd1f
edithturn <edithpuclla20@gmail.com>
2020-06-18 13:04:37 -0700

    :hushed: clean workspace

--- tests/main.c
+++ tests/main.c
@@ -20,18 +20,17 @@ int main(int argc, char **argv)
 		// Reads from a file
 		while ((ret = get_next_line(fd, &line)) > 0)
 		{
-			printf("[ MAIN while RET: %d] | A line has been read #%d => %s\n", ret, line_count, line);
+			printf(" \n [ Return: %d ] | A line has been read #%d => %s\n", ret, line_count, line);
 			line_count++;
 			free(line);
 		}
 
-		printf("\n");
-		printf("[MAIN Return: %d] A line has been read #%d: %s\n", ret, line_count++, line);
+		printf(" \n [ Return: %d ] A line has been read #%d: %s\n", ret, line_count++, line);
 		printf("\n");
 		if (ret == -1)
-			printf("-----------\n MAIN An error happened\n");
+			printf("-----------\n An error happened\n");
 		else if (ret == 0)
-			printf("-----------\n MAIN EOF has been reached\n");
+			printf("-----------\n EOF has been reached\n");
 		close(fd);
 	}
 	if (argc == 1)
@@ -42,9 +41,9 @@ int main(int argc, char **argv)
 			line_count++;
 		}
 		if (ret == -1)
-			printf("\nAn error happened\n");
+			printf("\n An error happened\n");
 		else if (ret == 0)
-			printf("\nEOF has been reached\n");
+			printf("\n EOF has been reached\n");
 		close(fd);
 	}
 	return (0);
--- tests/main_stdin.c
+++ tests/main_stdin.c
@@ -9,27 +9,23 @@ int main()
 	int line_count;
 	char *line;
 
-
 	line_count = 1;
 	ret = 0;
 	line = NULL;
 
 	// Read on stdin
-	printf("\n---Read from ---\n Please, enter your text:\n");
+	printf("Please, enter your text:\n");
 	while ((ret = get_next_line(0, &line)) > 0)
 	{
-		printf("[ MAIN while RET: %d] | A line has been read #%d => %s\n", ret, line_count, line);
+		printf(" \n [ Return: %d ] | A line has been read #%d => %s\n", ret, line_count, line);
 		line_count++;
 		free(line);
 	}
 
-	printf("\n");
-	printf("[MAIN Return: %d] A line has been read #%d: %s\n", ret, line_count++, line);
-	printf("\n");
 	if (ret == -1)
-		printf("-----------\n MAIN An error happened\n");
+		printf("\n MAIN An error happened\n");
 	else if (ret == 0)
-		printf("-----------\n MAIN EOF has been reached\n");
+		printf("\n MAIN EOF has been reached\n");
 
 	return (0);
 }
